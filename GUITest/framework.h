cd77034fe466a403934813ef0bc101c2f445c469
luqc <luqcxaj@163.com>
2020-05-17 18:13:57 +0800

    D2D demo

--- GUITest/framework.h
+++ GUITest/framework.h
@@ -1,10 +1,57 @@
 #pragma once
 
+#ifndef WINVER
+#define WINVER 0x0800
+#endif
+
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0800
+#endif
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
+#include <atlbase.h>
+#include <atlwin.h>
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
 // C RunTime Header Files
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
-#include <tchar.h>
+#include <wchar.h>
+#include <math.h>
+//#pragma comment(lib,"d2d1")
+
+template<class Interface>
+inline void SafeRelease(
+	Interface*& ppInterfaceToRelease) {
+	if (ppInterfaceToRelease != nullptr)
+	{
+		ppInterfaceToRelease->Release();
+		ppInterfaceToRelease = nullptr;
+	}
+
+}
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) if (!(b)) {OutputDebugStringA("Assert: " #b "\n");}
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
