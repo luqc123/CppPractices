d6134f97f39a5023bba5b1299bdd063394352961
luqc <luqcxaj@163.com>
2020-12-22 07:35:04 +0800

    replace empty

--- AlgorithmTest/AlgorithmTest.cpp
+++ AlgorithmTest/AlgorithmTest.cpp
@@ -1,13 +1,14 @@
 ﻿// AlgorithmTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <deque>
-#include "ReverseWordInSetence.hpp"
 #include <cstdio>
 #include <iostream>
+#include <string>
+#include "ReplaceEmpty.hpp"
 
 using namespace std;
 
 int main()
 {
-	Test1();
+	Test();
 }
--- AlgorithmTest/ReplaceEmpty.hpp
+++ AlgorithmTest/ReplaceEmpty.hpp
@@ -5,4 +5,20 @@
 using namespace std; 
 
 //Ìæ»»¿Õ¸ñÎªXXX
+void ReplaceSpace(char string[],int length) {
+	if (string == nullptr || length<=0) return;
+	int spaceNum = 0;
+	int noneSpaceNum = 0;
+	char* pBegin = string;
+	while (*pBegin != '\0') {
+		if (*pBegin == ' ') {
+			++spaceNum;
+		}
+		++noneSpaceNum;
+		++pBegin;
+	}
+	int totalLength > length 
+}
 
+void Test() {
+}
